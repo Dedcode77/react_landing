59bfb88231e13a273cdb3aa393f70fcddca2c8df
Dedcode77 <salifciss222@gmail.com>
2023-07-24 06:29:30 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import './App.css';
+import React from 'react';
+import './Services.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 
 function App() {
   return (
